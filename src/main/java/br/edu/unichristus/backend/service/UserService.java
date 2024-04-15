7ca895f8fe8d6c71364da6a467de3a6e43c47cff
marcosdsj <89165879+MarcosSilva2021@users.noreply.github.com>
2024-04-15 20:48:51 -0300

    update servece

--- src/main/java/br/edu/unichristus/backend/service/UserService.java
+++ src/main/java/br/edu/unichristus/backend/service/UserService.java
@@ -1,9 +1,11 @@
 package br.edu.unichristus.backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import br.edu.unichristus.backend.data.User;
+import br.edu.unichristus.backend.exception.CommonsException;
 import br.edu.unichristus.backend.repository.UserRepository;
 
 
@@ -17,39 +19,46 @@ public class UserService {
 
 	public User save(User user) {
 		if(user.getName().length() > 150) {
-			System.out.println("Limite de caracteres excedido!");
-			return null;
+			//System.out.println("Limite de caracteres excedido!");
+			throw new CommonsException(
+					HttpStatus.BAD_REQUEST,
+					"unchristus.backend.service.badrequest.exception",
+					"Limite de Carateres excedido"
+					);
+		}
+		var userFind = repository.findByLogin(user.getLogin());
+		if(!userFind.isEmpty()) {
+			throw new CommonsException(
+					HttpStatus.CONFLICT,
+					"unichristus.backend.service.user.conflict.exception",
+					"O Login informado já existe!"
+					);
 		}
 		
 		var userSaved = repository.save(user);
 		
 		return userSaved;
-	}
+	}	
 	
-	public User update(User user) {
-		if(user.getName().length() > 150) {
-			System.out.println("usuario editado!");
-			return null;
-		}
-		
-		var userSaved = repository.save(user);
-		
-		return userSaved;
-	}
 	
 	public List<User> listAll(){
 		return repository.findAll();
 	}
 	
 	public void delete(Long id) {
+		findById(id);
 		repository.deleteById(id);
 	}
 	
 	public User findById(long id) {
 		var user = repository.findById(id);
 		if(user.isEmpty()) {
-			System.out.println("usuario não encontrado");
-			return null;
+			//System.out.println("usuario não encontrado");
+			throw new CommonsException(
+					HttpStatus.NOT_FOUND,
+					"unchristus.backend.service.user.notfound.exception",
+					"O usuario com o id informado não foi encontrado"
+					);
 		}
 		return user.get();
 		
