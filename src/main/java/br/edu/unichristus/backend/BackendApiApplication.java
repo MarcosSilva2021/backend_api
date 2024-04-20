079df21288846767e0f974a12bf9a1dc9e2ef69b
marcosdsj <89165879+MarcosSilva2021@users.noreply.github.com>
2024-04-19 21:37:32 -0300

    add framework swagger - p/ documentar o codigo

--- src/main/java/br/edu/unichristus/backend/BackendApiApplication.java
+++ src/main/java/br/edu/unichristus/backend/BackendApiApplication.java
@@ -3,6 +3,25 @@ package br.edu.unichristus.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+	    info = @Info(
+	        title = "Backend da API construida em topicos e desenvolviemto WEB",
+	        version = "1.0",
+	        description = "Trata-se da Documentação da API construida em Sala de aula",
+	        contact = @Contact(
+	        	    name = "marcos ads",
+	        	    email = "msss@exemplo.com",
+	        	     url = "https://unichristus.edu.br")
+	      
+	        
+	    )
+	)
+
+
 @SpringBootApplication
 public class BackendApiApplication {
 
